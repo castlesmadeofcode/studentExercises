27168497c90e09506b864d2f366d1d69c9f53eaf
castle <castlesmadeofcode@gmail.com>
2020-04-15 15:25:32 -0500

    initial setup complete for Student Exercises - Part One

---
+++ exercise.py
@@ -0,0 +1,5 @@
+# You must define a type for representing an exercise in code.
+#  An exercise can be assigned to many students.
+
+# Name of exercise
+# Language of exercise (JavaScript, Python, CSharp, etc.)
